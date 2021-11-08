a0338509eac7fc5b9f163d928281165c949c6c7e
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-11-08 20:03:19 +0100

    transform data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,13 +15,20 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({ posts: response.data })
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map((post) => {
+                    return {
+                        ...post,
+                        author: 'Bella'
+                    }
+                });
+                this.setState({ posts: updatedPosts })
             });
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-                return <Post key={post.id} title={post.title}/>;
+                return <Post key={post.id} title={post.title} author={post.author}/>;
             }
         );
 
