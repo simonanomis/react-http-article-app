c5b6d341322b339926b0890b53997eccee4f7b2e
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-11-09 15:44:33 +0100

    fetching data on update

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+    
+    componentDidUpdate () {
+        if(this.props.id) {
+            //below is check so we do not have infinite loop. setState will trigger componentDidUpdate and
+            //this will trigger infinite loop of requests
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({loadedPost: response.data})
+                    });
+            }
+        }
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+
         if(this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+
+        if(this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
